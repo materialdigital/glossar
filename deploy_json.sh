b777d8440edd9db600f98a362d1d494025ce4e7f
Joerg Schaarschmidt <joerg.schaarschmidt@kit.edu>
2021-09-14 10:32:27 +0200

    update deploy script

--- deploy_json.sh
+++ deploy_json.sh
@@ -1,7 +1,11 @@
+#!/bin/bash
+set -e
+
 pip install -r requirements.txt
 python glossary.py
 git checkout json
 if diff glossary.json assets/glossary.json >/dev/null ; then
+  echo "json changed - updating repo"
   mv -v glossary.json assets
   git config --local user.email "action@github.com"
   git config --local user.name "GitHub Action"
